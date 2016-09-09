e5bb30205651e65f207db06aee33f562270a93c6
Derek Jay Harrison <harrisondj@dcsdk12.org>
2016-09-09 12:40:23 -0600

    Implement closer_to and not_string

--- quiz.py
+++ quiz.py
@@ -13,14 +13,15 @@ def not_string(str):
 	if str.startswith('not'):
 		return str + 'not'
 	else:
-		return 'not ' + str
+		return 'not' + str
 
-
-print not_string('not cool ') #cool not
+print not_string('not cool') #cool not
 print not_string('cool') #not cool
 
 def icy_hot(a,b):
-	if a < 0 or b > 100:
+	if a < 0 and b > 100:
+		return True
+	if a > 100 and b < 0:
 		return True
 	else:
 		return False
@@ -28,13 +29,19 @@ def icy_hot(a,b):
 print icy_hot(30,50)#F
 print icy_hot(30,200)#T
 print icy_hot(-1,20)#T
+print icy_hot(101,110)#F
 
-#def closer_to(t,a,b):
-	#if
+def closer_to(t,a,b):
+	if abs(t - a) < abs(t - b):
+		return a
+	if abs(t - b) < abs(t - a):
+		return b
+	if abs(t - a) == abs(t - b):
+		return 0
 
-#print closer_to(50,41,2)#41
-#print closer_to(50,2,25)#25
-#print closer_to(50,40,60)#0
+print closer_to(50,41,2)#41
+print closer_to(50,2,25)#25
+print closer_to(50,40,60)#0
 
 def two_as_one(a,b,c):
 	if a + b == c or a + c == b or b + c == a:
